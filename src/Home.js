5aa237ac67c38b1a4bf992692009b2d2eeeef5c9
vvr3ddy <vvr-rockstar@bmsit.in>
2021-08-10 09:16:37 +0530

    :octocat: :sparkles: Learn States - useState Hook
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // let name = "Mario";
+    const [name, setName] = useState("Mario");
+    const [age, setAge] = useState(12);
     const handleClick = (e) => {
-        // To avoid page reload when the button is clicked, useful in form submissions
-        e.preventDefault();
-        //  e -> Event object
-        console.log('Button Clicked', e);
+        setName("Luigi");
+        setAge(30);
     };
-    const handleClickAgain = (name, e) => {
-        // To avoid page reload when the button is clicked, useful in form submissions
-        console.log('Hello ' + name, e.target);
-    }
     return (
         <div className="home">
             <h2>Homepage</h2>
             {/* Don't invoke the function, just set a reference by calling the name only */}
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('VVR', e)}>Click Me Again</button>
-            {/* The anonymous function gets the event object by default instead of handleClickAgain */}
+            <p>{name} is {age} years old</p>
+
         </div>
     );
 }
