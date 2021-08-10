5ad43780ed2bf498860419e0e8d628790f198513
vvr3ddy <vvr-rockstar@bmsit.in>
2021-08-10 08:52:59 +0530

    :octocat: Handling Click Events
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,23 @@
 const Home = () => {
-    return ( 
+    const handleClick = (e) => {
+        // To avoid page reload when the button is clicked, useful in form submissions
+        e.preventDefault();
+        //  e -> Event object
+        console.log('Button Clicked', e);
+    };
+    const handleClickAgain = (name, e) => {
+        // To avoid page reload when the button is clicked, useful in form submissions
+        console.log('Hello ' + name, e.target);
+    }
+    return (
         <div className="home">
             <h2>Homepage</h2>
+            {/* Don't invoke the function, just set a reference by calling the name only */}
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('VVR', e)}>Click Me Again</button>
+            {/* The anonymous function gets the event object by default instead of handleClickAgain */}
         </div>
-     );
+    );
 }
- 
+
 export default Home;
