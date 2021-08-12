443c0bf84426b5819dba7bf3bbb671d97fe9a0ef
vvr3ddy <vvr-rockstar@bmsit.in>
2021-08-12 17:46:25 +0530

    :octocat: :sparkles: Functions as Props
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,8 @@
-const BlogList = (props) => {
-// const BlogList = (blogs, title) => {
+const BlogList = ({blogs, title, handleDelete}) => {
+    // const BlogList = (blogs, title) => {
     // Receiving the props from Home.js
-    const blogs = props.blogs;
-    const title = props.title;
+    // const blogs = props.blogs;
+    // const title = props.title;
 
     return (
         <div className="blog-list">
@@ -12,6 +12,7 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written By: {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                     <br />
                 </div>
             ))}
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
     // Props - Global scoped variables
-    
+
     const [blogs, setBlogs] = useState([
         { title: 'My First Blog', body: 'This is my first blog post', author: 'Mario', id: 1 },
         { title: 'My Second Blog', body: 'This is my second blog post', author: 'Luigi', id: 2 },
@@ -12,10 +12,15 @@ const Home = () => {
         { title: 'My Fifth Blog', body: 'This is my fifth blog post', author: 'Mushroom', id: 5 },
         { title: 'My Sixth Blog', body: 'This is my sixth blog post', author: 'Luigi', id: 6 },
     ]);
+
+    const handleDelete = (blogId) => {
+        const newBlogs = blogs.filter(blog => blog.id !== blogId);
+        setBlogs(newBlogs);
+    };
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs"/>
-            <BlogList blogs={blogs.filter((blog)=>blog.author==="Luigi")} title="Luigi's Blogs"/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
         </div>
     );
 }
