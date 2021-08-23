ab96ce617d006aa3827aff515f235abc7fcb6f40
vvr3ddy <vvr-rockstar@bmsit.in>
2021-08-23 17:36:27 +0530

    :octocat: :sparkles: Error Handling with JSON Server
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,31 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        setTimeout(() => {
+        // Set timeout is only for the sake of a demo to display loader
+        setTimeout(() => { 
             fetch('http://localhost:8000/blogs')
                 .then(response => {
-                    return response.json();
+                    if (response.ok)
+                        return response.json();
+                    else
+                    // To Simulate, set the fetch url to something invalid
+                        throw new Error('Bad response');
                 })
                 .then(data => {
                     console.log(data);
                     setIsPending(false);
                     setBlogs(data);
-                });
+                    setError(null);
+                })
+                .catch((err) => {
+                    // To Simulate, Stop the json server and refresh the main page
+                    setError(err.message);
+                    setIsPending(false);
+                })
         }, 1000);
 
     }, []);
@@ -27,6 +38,7 @@ const Home = () => {
             {/* Conditional Rendering
             if blogs is null/false, the right hand side of && is never rendered 
             */}
+            { error && <div> {error} </div> }
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
